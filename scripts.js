45be412419bf3f7aa5ad1fdc2cba6cf57f3f9904
drod702 <52800693+drod702@users.noreply.github.com>
2020-06-26 12:32:01 -0700

    Add files via upload
    Added date

--- scripts.js
+++ scripts.js
@@ -1,3 +1,9 @@
+const dateElement = document.getElementById("date");
+let today = new Date();
+let options = { weekday: 'long', month: 'short', day: 'numeric' };
+
+dateElement.innerHTML = today.toLocaleDateString("en-US", options);
+
 class ToDoClass {
     constructor() {
         // alert('Hello App User!');
@@ -84,6 +90,7 @@ class ToDoClass {
         this.loadTasks();
     }
 }
+
 let toDo;
 window.addEventListener("load", () => {
     toDo = new ToDoClass();
